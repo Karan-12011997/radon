e54428d91009578594b5c2137c465c1886c1f185
Karan-12011997 <karanjaiswal9199457372@gmail.com>
2022-06-02 21:12:35 +0530

    pritesh sir assignment missing no

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,26 +1,41 @@
 const express = require('express');
-const externalModule = require('./logger')
-const externalModule1 = require('../util/helper')
-const externalModule3 = require('../validator/formatter')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    externalModule.welCome()
-    externalModule1.printdate()
-    externalModule1.printMonth()
-    externalModule1.batchinfo()
-    externalModule3.dataTrim()
-    externalModule3.lowercase()
-    externalModule3.uppercase()
- 
-    
-    
-    res.send('My first ever api!')
+router.get('/sol1', function (req, res) {
+    let arr = [1, 2, 3, 5, 6, 7]
+
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+
+    let lastDigit = arr.pop()
+    let consecutiveSum = lastDigit * (lastDigit + 1) / 2
+    let missingNumber = consecutiveSum - total
+
+
+
+    res.send({ data: missingNumber })
 });
+
+router.get('/sol2', function (req, res) {
+    let arr= [33, 34, 35, 37, 38]
+    total=0;
+    for (var i in arr){
+        total+=arr[i]
+    };
+    let a=arr.pop()
+    let addTotal=a*(a+1)/2
+    let b=arr.shift()
+    let addFirst=(b-1)*(b)/2
+    let total1=addTotal-addFirst
+    let missingNumber=total1-total
+
+
+
+    res.send({data:missingNumber})
+})
 
 
 module.exports = router;
