dc86eee61ad7018b767439e5eb826d260e010b66
Karan-12011997 <karanjaiswal9199457372@gmail.com>
2022-06-02 18:44:29 +0530

    updated

--- src/validator/formatter.js
+++ src/validator/formatter.js
@@ -1,17 +1,24 @@
 const dataTrim=function(){
     const d= "    Karan Jaiswal     "
+    console.log("code before=")
+    console.log(d)
     console.log( d.trim())
 }
 module.exports.dataTrim = dataTrim
 
 const lowercase=function(){
     const d= "  FunCTION UP    "
-    console.log( d.toLowerCase(),"code before"+d)
+    console.log("code before ="+d)
+
+    console.log( d.toLowerCase())
 }
 module.exports.lowercase = lowercase
 
 const uppercase=function(){
     const d= "  FunCTION UP    "
-    console.log( d.toUpperCase(),"code before"+d)
+    console.log("code before ="+d)
+    console.log( d.toUpperCase())
 }
 module.exports.uppercase = uppercase
+
+
